d6214cf632a3b79bc53b46e095e7cc961e24c2e4
Ben Castan <ben@castanfamily.com>
2017-07-25 09:18:32 +1000

    Tidy up and some refatoring

--- cluster_response_test.py
+++ cluster_response_test.py
@@ -10,55 +10,55 @@ URL_LIST = ['https://www.pinkfrosting.com.au', 'https://www.mcas.com.au',
             'https://www.sitesuite.com.au']
 
 
-class PageSpeedOnLine:
-    #Add sites here, this list is not a definativ elist
+class pageSpeedOnLine:
+    # Add sites here, this list is not a definativ elist
 
 
     # Gets ALL results for a given site
     # Returns a json object from google
-    def GetSiteResults(self,target_url):
+    def getSiteResults(self, target_url):
         params = {'url': target_url,
-                    'key': 'AIzaSyA6pibr5UDIdt6Qxq1uoNOeiRbo_L3Kizw'}
+                  'key': 'AIzaSyA6pibr5UDIdt6Qxq1uoNOeiRbo_L3Kizw'}
         url = ('https://www.googleapis.com/pagespeedonline/v1/runPagespeed?' +
-                urllib.urlencode(params))
+               urllib.urlencode(params))
         response = urllib.urlopen(url).read()
         return response
 
     # Gets the page speed for a single site
     # returns the score as a string as the json returnedby google is converted to a
     # python dictionary for easy manipulation
-    def GetPageSpeedResults(self,target_url):
-        site_speed = json.loads(self.GetSiteResults(target_url))
+    def getPageSpeedResults(self, target_url):
+        site_speed = json.loads(self.getSiteResults(target_url))
         return site_speed.get('score')
 
-    def GetClusterAverage(self):
+    def getClusterAverage(self):
         total_cluster_speed = 0
         count = 1
-        print (Fore.MAGENTA + 'Analysing Cluster speed')
-        print (Fore.MAGENTA + 'Only returns Cluster Average')
+        print(Fore.MAGENTA + 'Analysing Cluster speed')
+        print(Fore.MAGENTA + 'Only returns Cluster Average')
         for TEST_URL in URL_LIST:
-            site_speed = self.GetPageSpeedResults(TEST_URL)
+            site_speed = self.getPageSpeedResults(TEST_URL)
             # display(TEST_URL + " ", site_speed)
             b = 'Averaging' + '.' * count
             print('\r'),
-            print ('\r' + b),
+            print('\r' + b),
             total_cluster_speed = (total_cluster_speed + site_speed)
             count = count + 1
 
         # print total_cluster_speed
         # print len(URL_LIST)
         average_cluster_speed = (int(total_cluster_speed)) / len(URL_LIST)
-        print ""
+        print("")
         message = "Averaged Cluster Speed = "
         self.display(message, average_cluster_speed)
 
-    def GetSiteAverage(self):
+    def getSiteAverage(self):
         total_cluster_speed = 0
 
-        print (Fore.MAGENTA + 'Analysing Cluster speed')
-        print (Fore.MAGENTA + 'Returns each site result and Cluster average')
+        print(Fore.MAGENTA + 'Analysing Cluster speed')
+        print(Fore.MAGENTA + 'Returns each site result and Cluster average')
         for TEST_URL in URL_LIST:
-            site_speed = self.GetPageSpeedResults(TEST_URL)
+            site_speed = self.getPageSpeedResults(TEST_URL)
             self.display(TEST_URL + " ", site_speed)
             # print TEST_URL + " Speed %s " % cluster_speed
             total_cluster_speed = total_cluster_speed + site_speed
@@ -69,22 +69,22 @@ class PageSpeedOnLine:
         message = "Averaged Cluster Speed = "
         self.display(message, average_cluster_speed)
 
-    #A worker sub to display the results with colour.
+    # A worker sub to display the results with colour.
     def display(self, message, speed):
         speedOk = 75
         speedHmm = 50
         speedBad = 35
         if speed < speedBad:
-            print (Fore.RED + message + str(speed))
+            print(Fore.RED + message + str(speed))
         elif speed >= speedHmm and speed < speedOk:
             print(Fore.YELLOW + message + str(speed))
         elif speed >= speedOk:
             print(Fore.GREEN + message + str(speed))
-        #print (textColour +  message + str(speed))
+            # print (textColour +  message + str(speed))
 
 
 if __name__ == '__main__':
-    #Make an instance of the Class.
-    psol = PageSpeedOnLine()
-    #psol.GetSiteAverage()
-    psol.GetClusterAverage()
+    # Make an instance of the Class.
+    psol = pageSpeedOnLine()
+    # psol.getSiteAverage()
+    psol.getClusterAverage()
